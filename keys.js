ec70b8f893f1f4cf1087beb539a30bf8e51bce78
Nandha <nandhakumar.selvaraju@kovai.co>
2020-04-22 17:08:14 +0000

    =inintal commit

---
+++ keys.js
@@ -0,0 +1,16 @@
+const isDev = (process.env.NODE_ENV || 'development') !== 'production';
+
+const baseUrl = isDev
+  ? "https://037ea086.ngrok.io"
+  : "https://kovaid3603.herokuapp.com";
+
+module.exports = {
+  baseUrl: baseUrl,
+  crowdinClientId: process.env.CROWDIN_CLIENT_ID || "6M8zDGaKumnuxbO3dJ9u",
+  crowdinClientSecret: process.env.CROWDIN_CLIENT_SECRET || "BfHocTR480zCNeSfi76hoHa8YVONOlywPJtePeKR",
+  integrationClientId: process.env.INTEGRATION_CLIENT_ID || "776140467595",
+  integrationSecret: process.env.INTEGRATION_CLIENT_SECRET || "ab847d4e5e756e2d7752cd647f06a66273a3a0ca79ab72d0a8",
+  callbackUrl: baseUrl + "/integration-token",
+  cryptoSecret: process.env.CRYPTO_SECRET || 'UniqueCryptoSecret',
+  crowdinAuthUrl : "https://accounts.crowdin.com/oauth/token"
+};
