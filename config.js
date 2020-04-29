4053637c901c6c5369ea5d237f58e020dc069b49
Nandha <nandhakumar.selvaraju@kovai.co>
2020-04-29 13:00:17 +0000

    manifest changes for ngrok

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const keys = require('./keys');
 
 const manifest = {
-  "identifier": "document360-app",
-  "name": "Document360",
+  "identifier": "document360-app-dev",
+  "name": "Document360-dev",
   "baseUrl": keys.baseUrl,
   "authentication": {
       "type": "authorization_code",
@@ -17,8 +17,8 @@ const manifest = {
   "modules": {
       "integrations": [
           {
-              "key": "document360_app_test",
-              "name": "Document360",
+              "key": "document360_app_test_dev",
+              "name": "Document360-dev",
               "description": "Upload and localize your content from Document360",
               "logo": "/assets/logo.svg",
               "url": "/"
