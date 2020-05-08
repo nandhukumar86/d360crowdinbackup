c19922f91bbb0b637bb0ffa7f0dfc3f5a92e6cd2
Nandha <nandhakumar.selvaraju@kovai.co>
2020-05-08 09:24:19 +0000

    Partial completion of version and branching

--- uploadToIntegration.js
+++ uploadToIntegration.js
@@ -49,6 +49,18 @@ function integrationUpdate() {
 
 module.exports = integrationUpdate;
 
+function ParseFileName(filename) {
+  var split1 = filename.split("_")
+  var split2 = split1[2].split(".")
+
+  return {
+    slugName: split1[0],
+    fileTranlationType: split1[1],
+    articleId: split2[0],
+    extension: split2[1]
+  };
+}
+
 const prepareData = (filesTranslations, translations, res) => {
   return new Promise((resolve, reject) => {
 
@@ -77,12 +89,13 @@ const prepareData = (filesTranslations, translations, res) => {
             filesById = responses.reduce((acc, fileData) => ({ ...acc, [`${fileData.data.id}`]: fileData.data }), {});
             // Get all selected files source campaigns
             return Promise.all(Object.values(filesById).map(f => {
-              if (f.name.indexOf(`_content.${f.type}`) > 0) {
-                return integrationApiClient.get(`/Articles/${f.name.replace(`_content.${f.type}`, '')}`)
-              }
-              else if (f.name.indexOf(`_title.${f.type}`) > 0) {
-                return integrationApiClient.get(`/Articles/${f.name.replace(`_title.${f.type}`, '')}`)
-              }
+              return integrationApiClient.get(`/Articles/${ParseFileName(f.name).articleId}`)
+              // if (f.name.indexOf(`_content.${f.type}`) > 0) {
+              //   return integrationApiClient.get(`/Articles/${f.name.replace(`_content.${f.type}`, '')}`)
+              // }
+              // else if (f.name.indexOf(`_title.${f.type}`) > 0) {
+              //   return integrationApiClient.get(`/Articles/${f.name.replace(`_title.${f.type}`, '')}`)
+              // }
             }))
           })
           .then(integrationFiles => {
@@ -109,25 +122,27 @@ const prepareData = (filesTranslations, translations, res) => {
 
 const updateIntegrationFile = (params) => {
   const { filesById, integrationFilesById, integrationFilesList, translatedFilesData, t, index, res } = params;
-  const crowdinFileName = filesById[t.fileId].name;
-  const fileName = `${filesById[t.fileId].title}`;//${t.languageId}`; // prepare file translation name
-  const integrationTranslationFile = integrationFilesList.find(f => f.slug === fileName.replace(`_content.${f.type}`, '') || f.slug === fileName.replace(`_title.${f.type}`, '')); // Try find translation on
+  const crowdinFileName = filesById[t.fileId].name; //${t.languageId}`; // prepare file translation name
   const integrationApiClient = d360Instance;
+
+  const _obj = ParseFileName(crowdinFileName);
+  const integrationTranslationFile = integrationFilesList.find(f => f.id == _obj.articleId); // Try find translation on
 
   if (integrationTranslationFile) {
     // We find translation for this file and this language, update it
     //return integrationApiClient.put('/Articles/' + integrationTranslationFile.id, {content:'this needs the from code'});
-    if (crowdinFileName.indexOf("_content.") > 0) {
-      if (crowdinFileName.indexOf(".md") > 0) {
-        return integrationApiClient.put('/Articles/' + integrationTranslationFile.id, { content: translatedFilesData[index] })
-      }
-      else if (crowdinFileName.indexOf(".html") > 0) {
-        return integrationApiClient.put('/Articles/' + integrationTranslationFile.id, { html_content: translatedFilesData[index] })
-      }
+    var payload = {};
+
+    if (_obj.fileTranlationType == "content" && _obj.extension == "md") {
+      payload["content"] = translatedFilesData[index];
     }
-    if (crowdinFileName.indexOf("_title.") > 0) {
-      return integrationApiClient.put('/Articles/' + integrationTranslationFile.id, { title: translatedFilesData[index] })
+    else if (_obj.fileTranlationType == "content" && _obj.extension == "html") {
+      payload["html_content"] = translatedFilesData[index];
     }
+    else if (_obj.fileTranlationType == "title" && _obj.extension == "txt") {
+      payload["title"] = translatedFilesData[index];
+    }
+    return integrationApiClient.put('/Articles/' + integrationTranslationFile.id, payload);
   }
   // else {
   //   // We don't find translation for this file and language
